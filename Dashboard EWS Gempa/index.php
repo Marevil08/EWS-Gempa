244ba580a3d946d0faa1f9780688c99113d36e7c
Marevil08 <130815167+Marevil08@users.noreply.github.com>
2023-11-17 18:41:51 +0700

    Add files via upload

---
+++ Dashboard EWS Gempa/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Website Early Warning</title>
+    <link rel="stylesheet" type="text/css" href="style.css" />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+    />
+
+    <div class="icon-bar">
+      <a class="active" href="index.php"><i class="fa fa-home"></i></a>
+      <a class="mail" href="mailto:shintameidytaputri@gmail.com"
+        ><i class="fa fa-envelope"></i
+      ></a>
+      <a class="ig" href="https://www.instagram.com/gunadarma/"
+        ><i class="fa fa-instagram"></i
+      ></a>
+      <a class="print" href=""><i class="fa fa-cloud-download"></i></a>
+      <!-- <a href="#" class="icon-bar-img"> </a> -->
+      <a href="#" class="icon-bar-mer">
+        <img src="gundar.png" />
+        <img src="merdeka.png" />
+        <img width="200px" src="ugtechnopark.png" />
+      </a>
+    </div>
+  </head>
+  <body>
+    <div class="container">
+      <div class="subheading">Welcome to Our Website</div>
+      <div class="heading">Early Warning System for Earthquake</div>
+      <br />
+      <br />
+      <a href="chart.php" class="btn">Monitoring Dashboard</a>
+    </div>
+  </body>
+</html>
